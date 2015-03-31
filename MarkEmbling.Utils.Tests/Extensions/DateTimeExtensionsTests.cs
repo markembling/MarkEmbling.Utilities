a48e1f06fc53d1a0d4a7a0294aefdf4bbcd03178
Mark Embling <mark@markembling.info>
2015-03-31 10:36:43 +0100

    Add DateTime extensions with GetShortDate method

---
+++ MarkEmbling.Utils.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MarkEmbling.Utils.Extensions;
+using NUnit.Framework;
+
+namespace MarkEmbling.Utils.Tests.Extensions {
+    [TestFixture]
+    public class DateTimeExtensionsTests {
+        [Test]
+        public void GetShortDate_returns_correct_integer_representation() {
+            Assert.AreEqual(15, new DateTime(2015, 1, 1).GetShortYear());
+        }
+    }
+}
---
+++ MarkEmbling.Utils/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MarkEmbling.Utils.Extensions {
+    public static class DateTimeExtensions {
+        /// <summary>
+        /// Gets the two digit version of the year from a DateTime instance.
+        /// </summary>
+        /// <param name="dt">DateTime value</param>
+        /// <returns>Short year (e.g. 15 for 2015)</returns>
+        public static int GetShortYear(this DateTime dt) {
+            return Convert.ToInt32(dt.ToString("yy"));
+        }
+    }
+}
