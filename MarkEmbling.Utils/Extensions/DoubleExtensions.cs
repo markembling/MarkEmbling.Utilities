a466ca0f09ea8f5a2565b60a5ab5e516b7121a10
Mark Embling <mark@markembling.info>
2016-09-16 09:15:40 +0100

    Add rough comparer methods for floating-point numbers

--- MarkEmbling.Utils/Extensions/DoubleExtensions.cs
+++ MarkEmbling.Utils/Extensions/DoubleExtensions.cs
@@ -10,12 +10,23 @@ namespace MarkEmbling.Utils.Extensions {
         /// <returns>Integer representation</returns>
         public static int ToInt(this double value, ToIntMethod method) {
             if (method == ToIntMethod.IgnoreFraction)
-                return (int)value;
+                return (int) value;
 
             if (method == ToIntMethod.Round)
                 return Convert.ToInt32(value);
 
             throw new InvalidOperationException();
+        }
+
+        /// <summary>
+        /// Determine if a double is equivalent to the given double up to the level of precision provided
+        /// </summary>
+        /// <param name="double1">Double to compare</param>
+        /// <param name="double2">Double to compare against</param>
+        /// <param name="precision">Precision (number which defines the difference between the numbers which is considered different)</param>
+        /// <returns>True if considered equivalent</returns>
+        public static bool Equivalent(this double double1, double double2, double precision) {
+            return Math.Abs(double1 - double2) <= precision;
         }
     }
 }
--- MarkEmbling.Utils/Extensions/FloatExtensions.cs
+++ MarkEmbling.Utils/Extensions/FloatExtensions.cs
@@ -17,5 +17,16 @@ namespace MarkEmbling.Utils.Extensions {
 
             throw new InvalidOperationException();
         }
+
+        /// <summary>
+        /// Determine if a float is equivalent to the given float up to the level of precision provided
+        /// </summary>
+        /// <param name="double1">Float to compare</param>
+        /// <param name="double2">Float to compare against</param>
+        /// <param name="precision">Precision (number which defines the difference between the numbers which is considered different)</param>
+        /// <returns>True if considered equivalent</returns>
+        public static bool Equivalent(this float double1, float double2, float precision) {
+            return Math.Abs(double1 - double2) <= precision;
+        }
     }
 }
