cb4868f276c23bc9e1b6c1a0e72e13613d4b1b5e
Mark Embling <mark@markembling.info>
2016-10-23 17:43:56 +0100

    Add StreamReader.ReadAllLines extension method

---
+++ MarkEmbling.Utils/Extensions/StreamReaderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MarkEmbling.Utils.Extensions {
+    public static class StreamReaderExtensions {
+        /// <summary>
+        /// Reads all available lines from the current stream
+        /// </summary>
+        /// <param name="reader">StreamReader instance</param>
+        /// <returns>All lines read from the stream</returns>
+        public static IEnumerable<string> ReadAllLines(this StreamReader reader) {
+            string line;
+            while ((line = reader.ReadLine()) != null) {
+                yield return line;
+            }
+        }
+    }
+}
