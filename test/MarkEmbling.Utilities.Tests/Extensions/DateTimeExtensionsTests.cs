c3112957b75b178b79f49fb090f27a17c3001edb
Mark Embling <mark@markembling.info>
2017-04-25 18:50:25 +0100

    Fix a couple of incorrect names in the tests

--- test/MarkEmbling.Utilities.Tests/Extensions/DateTimeExtensionsTests.cs
+++ test/MarkEmbling.Utilities.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 namespace MarkEmbling.Utilities.Tests.Extensions {
     public class DateTimeExtensionsTests {
         [Fact]
-        public void GetShortDate_returns_correct_integer_representation() {
+        public void GetShortYear_returns_correct_integer_representation() {
             Assert.Equal(15, new DateTime(2015, 1, 1).GetShortYear());
         }
     }
