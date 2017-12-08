d9b6aa8e9d44181f2129073518da63ea0679a9d4
Mark Embling <mark@markembling.info>
2017-12-08 20:54:59 +0000

    Add extension methods to convert datetimes to unix timestamps

--- src/MarkEmbling.Utilities/Extensions/DateTimeExtensions.cs
+++ src/MarkEmbling.Utilities/Extensions/DateTimeExtensions.cs
@@ -2,6 +2,8 @@
 
 namespace MarkEmbling.Utilities.Extensions {
     public static class DateTimeExtensions {
+        private static readonly DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
         /// <summary>
         /// Gets the two digit version of the year from a DateTime instance
         /// </summary>
@@ -9,6 +11,25 @@ namespace MarkEmbling.Utilities.Extensions {
         /// <returns>Short year (e.g. 15 for 2015)</returns>
         public static int GetShortYear(this DateTime dt) {
             return Convert.ToInt32(dt.ToString("yy"));
+        }
+
+        /// <summary>
+        /// Get the Unix timestamp (seconds since 1970-01-01 00:00:00) from a DateTime instance
+        /// </summary>
+        /// <param name="dt">Current DateTime instance</param>
+        /// <returns>Seconds since Unix epoch</returns>
+        public static long GetUnixTimeSeconds(this DateTime dt) {
+            return Convert.ToInt64((dt.ToUniversalTime() - UNIX_EPOCH).TotalSeconds);
+        }
+
+        /// <summary>
+        /// Get the Unix timestamp in milliseconds (total milliseconds since 1970-01-01 00:00:00) from a DateTime
+        /// instance
+        /// </summary>
+        /// <param name="dt">Current DateTime instance</param>
+        /// <returns>Milliseconds since Unix epoch</returns>
+        public static long GetUnixTimeMilliseconds(this DateTime dt) {
+            return Convert.ToInt64((dt.ToUniversalTime() - UNIX_EPOCH).TotalMilliseconds);
         }
     }
 }
--- test/MarkEmbling.Utilities.Tests/Extensions/DateTimeExtensionsTests.cs
+++ test/MarkEmbling.Utilities.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -8,5 +8,35 @@ namespace MarkEmbling.Utilities.Tests.Extensions {
         public void GetShortYear_returns_correct_integer_representation() {
             Assert.Equal(15, new DateTime(2015, 1, 1).GetShortYear());
         }
+
+        [Fact]
+        public void GetUnixTimeSeconds_returns_correct_value_for_epoch_date() {
+            Assert.Equal(0, new DateTime(1970, 1, 1).GetUnixTimeSeconds());
+        }
+
+        [Fact]
+        public void GetUnixTimeSeconds_returns_correct_value_for_before_epoch_date() {
+            Assert.Equal(-86400, new DateTime(1969, 12, 31).GetUnixTimeSeconds());
+        }
+
+        [Fact]
+        public void GetUnixTimeSeconds_returns_correct_value_for_after_epoch_date() {
+            Assert.Equal(86400, new DateTime(1970, 1, 2).GetUnixTimeSeconds());
+        }
+
+        [Fact]
+        public void GetUnixTimeMilliseconds_returns_correct_value_for_epoch_date() {
+            Assert.Equal(0, new DateTime(1970, 1, 1).GetUnixTimeMilliseconds());
+        }
+
+        [Fact]
+        public void GetUnixTimeMilliseconds_returns_correct_value_for_before_epoch_date() {
+            Assert.Equal(-86400000, new DateTime(1969, 12, 31).GetUnixTimeMilliseconds());
+        }
+
+        [Fact]
+        public void GetUnixTimeMilliseconds_returns_correct_value_for_after_epoch_date() {
+            Assert.Equal(86400000, new DateTime(1970, 1, 2).GetUnixTimeMilliseconds());
+        }
     }
 }
