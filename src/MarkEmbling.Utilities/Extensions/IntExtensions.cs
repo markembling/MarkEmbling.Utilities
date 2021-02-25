90f01badceda15be8ed133493b761e5a5d9e2937
Mark Embling <mark@markembling.info>
2021-02-25 14:32:16 +0000

    Add ability to get numeric ordinals
    Includes abilities to get both the prefix itself for a given number (e.g. 1 => "st"), along with a string containing the number and the ordinal prefix together (e.g. 1 => "1st").

---
+++ src/MarkEmbling.Utilities/Extensions/IntExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace MarkEmbling.Utilities.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Return a string representation of this number as an ordinal numeral
+        /// </summary>
+        /// <param name="value">Current integer instance</param>
+        /// <returns>Ordinal numeral</returns>
+        public static string ToOrdinal(this int value)
+        {
+            return ToOrdinal(value, null);
+        }
+
+        /// <summary>
+        /// Return a string representation of this number as an ordinal numeral
+        /// </summary>
+        /// <param name="value">Current integer instance</param>
+        /// <param name="format">A standard or custom numeric format string</param>
+        /// <returns>Ordinal numeral</returns>
+        public static string ToOrdinal(this int value, string format)
+        {
+            var number = !string.IsNullOrEmpty(format) ? value.ToString(format) : value.ToString();
+            var suffix = GetOrdinalSuffix(value);
+            return $"{number}{suffix}";
+        }
+
+        /// <summary>
+        /// Return a string containing the appropriate ordinal suffix for the number
+        /// </summary>
+        /// <param name="value">Current integer instance</param>
+        /// <returns>Ordinal suffix</returns>
+        public static string GetOrdinalSuffix(this int value)
+        {
+            var lastTwoDigits = value % 100;
+            if (lastTwoDigits < 11 || lastTwoDigits > 13)
+            {
+                var lastDigit = value % 10;
+                if (lastDigit == 1) return "st";
+                if (lastDigit == 2) return "nd";
+                if (lastDigit == 3) return "rd";
+            }
+            return "th";
+        }
+    }
+}
---
+++ tests/MarkEmbling.Utilities.Tests/Extensions/IntExtensionsTests.cs
@@ -0,0 +1,172 @@
+﻿using MarkEmbling.Utilities.Extensions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MarkEmbling.Utilities.Tests.Extensions
+{
+    public class IntExtensionsTests
+    {
+        [Fact]
+        public void GetOrdinalSuffix_handles_first_correctly()
+        {
+            Assert.Equal("st", 1.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void GetOrdinalSuffix_handles_second_correctly()
+        {
+            Assert.Equal("nd", 2.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void GetOrdinalSuffix_handles_third_correctly()
+        {
+            Assert.Equal("rd", 3.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void GetOrdinalSuffix_returns_th_for_all_single_digits_above_three()
+        {
+            const string expected = "th";
+            Assert.Equal(expected, 4.GetOrdinalSuffix());
+            Assert.Equal(expected, 5.GetOrdinalSuffix());
+            Assert.Equal(expected, 6.GetOrdinalSuffix());
+            Assert.Equal(expected, 7.GetOrdinalSuffix());
+            Assert.Equal(expected, 8.GetOrdinalSuffix());
+            Assert.Equal(expected, 9.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void GetOrdinalSuffix_returns_st_for_larger_numbers()
+        {
+            const string expected = "st";
+            Assert.Equal(expected, 21.GetOrdinalSuffix());
+            Assert.Equal(expected, 31.GetOrdinalSuffix());
+            Assert.Equal(expected, 41.GetOrdinalSuffix());
+            Assert.Equal(expected, 51.GetOrdinalSuffix());
+            Assert.Equal(expected, 61.GetOrdinalSuffix());
+            Assert.Equal(expected, 71.GetOrdinalSuffix());
+            Assert.Equal(expected, 81.GetOrdinalSuffix());
+            Assert.Equal(expected, 91.GetOrdinalSuffix());
+            Assert.Equal(expected, 101.GetOrdinalSuffix());
+            Assert.Equal(expected, 121.GetOrdinalSuffix());
+            Assert.Equal(expected, 991.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void GetOrdinalSuffix_returns_nd_for_larger_numbers()
+        {
+            const string expected = "nd";
+            Assert.Equal(expected, 22.GetOrdinalSuffix());
+            Assert.Equal(expected, 32.GetOrdinalSuffix());
+            Assert.Equal(expected, 42.GetOrdinalSuffix());
+            Assert.Equal(expected, 52.GetOrdinalSuffix());
+            Assert.Equal(expected, 62.GetOrdinalSuffix());
+            Assert.Equal(expected, 72.GetOrdinalSuffix());
+            Assert.Equal(expected, 82.GetOrdinalSuffix());
+            Assert.Equal(expected, 92.GetOrdinalSuffix());
+            Assert.Equal(expected, 102.GetOrdinalSuffix());
+            Assert.Equal(expected, 122.GetOrdinalSuffix());
+            Assert.Equal(expected, 992.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void ToOrdinal_returns_rd_suffix_correctly_for_larger_numbers()
+        {
+            const string expected = "rd";
+            Assert.Equal(expected, 23.GetOrdinalSuffix());
+            Assert.Equal(expected, 33.GetOrdinalSuffix());
+            Assert.Equal(expected, 43.GetOrdinalSuffix());
+            Assert.Equal(expected, 53.GetOrdinalSuffix());
+            Assert.Equal(expected, 63.GetOrdinalSuffix());
+            Assert.Equal(expected, 73.GetOrdinalSuffix());
+            Assert.Equal(expected, 83.GetOrdinalSuffix());
+            Assert.Equal(expected, 93.GetOrdinalSuffix());
+            Assert.Equal(expected, 103.GetOrdinalSuffix());
+            Assert.Equal(expected, 123.GetOrdinalSuffix());
+            Assert.Equal(expected, 993.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void ToOrdinal_returns_th_for_all_teens()
+        {
+            const string expected = "th";
+            Assert.Equal(expected, 10.GetOrdinalSuffix());
+            Assert.Equal(expected, 11.GetOrdinalSuffix());
+            Assert.Equal(expected, 12.GetOrdinalSuffix());
+            Assert.Equal(expected, 13.GetOrdinalSuffix());
+            Assert.Equal(expected, 14.GetOrdinalSuffix());
+            Assert.Equal(expected, 15.GetOrdinalSuffix());
+            Assert.Equal(expected, 16.GetOrdinalSuffix());
+            Assert.Equal(expected, 17.GetOrdinalSuffix());
+            Assert.Equal(expected, 18.GetOrdinalSuffix());
+            Assert.Equal(expected, 19.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void ToOrdinal_returns_th_for_larger_numbers_ending_in_teens()
+        {
+            const string expected = "th";
+            Assert.Equal(expected, 10010.GetOrdinalSuffix());
+            Assert.Equal(expected, 10011.GetOrdinalSuffix());
+            Assert.Equal(expected, 10012.GetOrdinalSuffix());
+            Assert.Equal(expected, 10013.GetOrdinalSuffix());
+            Assert.Equal(expected, 10014.GetOrdinalSuffix());
+            Assert.Equal(expected, 10015.GetOrdinalSuffix());
+            Assert.Equal(expected, 10016.GetOrdinalSuffix());
+            Assert.Equal(expected, 10017.GetOrdinalSuffix());
+            Assert.Equal(expected, 10018.GetOrdinalSuffix());
+            Assert.Equal(expected, 10019.GetOrdinalSuffix());
+        }
+
+        [Fact]
+        public void ToOrdinal_uses_whatever_ToString_provides_before_suffix()
+        {
+            var integerStr = 123456.ToString();
+            var ordinalStr = 123456.ToOrdinal();
+            var ordinalStrWithoutSuffix = ordinalStr.Substring(0, ordinalStr.Length - 2);
+            Assert.Equal(integerStr, ordinalStrWithoutSuffix);
+        }
+
+        [Fact]
+        public void ToOrdinal_uses_provided_format_string_for_number_before_suffix()
+        {
+            // The format used in this test is somewhat nonsensical
+            var ordinalStr = 123456.ToOrdinal("00000000");
+            var ordinalStrWithoutSuffix = ordinalStr.Substring(0, ordinalStr.Length - 2);
+            Assert.Equal("00123456", ordinalStrWithoutSuffix);
+        }
+
+        [Fact]
+        public void ToOrdinal_adds_suffix_when_using_format_string()
+        {
+            // The format used in this test is somewhat nonsensical
+            Assert.Equal("00123456th", 123456.ToOrdinal("00000000"));
+        }
+
+        [Fact]
+        public void ToOrdinal_uses_expected_suffixes()
+        {
+            var testCases = new Dictionary<int, string> {
+                { 1, "st" },
+                { 2, "nd" },
+                { 3, "rd" },
+                { 4, "th" },
+                { 10, "th" },
+                { 11, "th" },
+                { 21, "st" },
+                { 22, "nd" },
+                { 23, "rd" },
+                { 24, "th" }
+            };
+
+            foreach (var test in testCases)
+            {
+                var expected = test.Key.GetOrdinalSuffix();
+                var ordinalStr = test.Key.ToOrdinal();
+                var actual = ordinalStr.Last(2);
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
